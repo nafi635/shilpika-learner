1ae1c398f6d055b9608a6fa6080718837836b7ad
Ganga Silpika <swarnasilpika@gmail.com>
2023-09-20 13:58:26 -0500

    Completed pairs question from Day18Package

---
+++ Day18Package/PairsOfMenWomen.java
@@ -0,0 +1,27 @@
+package Day18Package;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PairsOfMenWomen {
+    public static void main(String[] args) {
+        ArrayList<String> man = new ArrayList<String>();
+        ArrayList<String> woman = new ArrayList<String>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 3 names of woman");
+        for (int i = 0; i < 3; i++) {
+            String nameWoman = sc.next();
+            woman.add(nameWoman);
+        }
+        System.out.println("Enter 3 names of man");
+        for (int i = 0; i < 3; i++) {
+            String nameMan = sc.next();
+            man.add(nameMan);
+        }
+        if(woman.size() == man.size()){
+            for(int i = 0;i<3;i++)
+            System.out.println("[" + woman.get(i) + " ," + man.get(i)  + "]");
+        }
+    }
+}
