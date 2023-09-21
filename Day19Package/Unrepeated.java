304b95ccac85906e9a5df6d280454240d400a978
Ganga Silpika <swarnasilpika@gmail.com>
2023-09-21 12:57:00 -0500

    Completed a  question from both Day19Package and Day20Package

---
+++ Day19Package/Unrepeated.java
@@ -0,0 +1,19 @@
+package Day19Package;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+
+public class Unrepeated {
+    public static void main(String[] args) {
+        HashSet<Character> unrepeated = new HashSet<>();
+        char[] storeChars = new char[10];
+        char[] j = new char[10];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please give a word");
+        String word = sc.next();
+            j = word.toCharArray();
+            System.out.println(j);
+        }
+}
---
+++ Day20Package/HashSetExample.java
@@ -0,0 +1,15 @@
+package Day20Package;
+
+import java.util.HashSet;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        HashSet<Integer> hashExample = new HashSet<>();
+        hashExample.add(1);
+        hashExample.add(2);
+        hashExample.add(1);
+        hashExample.add(2);
+        System.out.println(hashExample);
+        System.out.println(hashExample.contains(1));
+    }
+}
